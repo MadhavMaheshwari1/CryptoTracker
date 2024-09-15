30dd639245580ab657dfa91e803977acb5f14742
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-09-15 14:08:33 +0530

    Made some changes

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -5,6 +5,8 @@ import { ThemeContext } from '../context/ThemeContext';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FaRegStar } from "react-icons/fa6";
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { FaArrowTrendDown } from "react-icons/fa6";
 
 const Dashboard = () => {
   const [cryptoData, setCryptoData] = useState([]);
@@ -20,6 +22,7 @@ const Dashboard = () => {
     const fetchCryptoData = async () => {
       const cachedData = localStorage.getItem('cryptoData');
       if (cachedData) {
+        console.log(JSON.parse(cachedData));
         setCryptoData(JSON.parse(cachedData));
         setLoading(false);
         return;
@@ -93,7 +96,7 @@ const Dashboard = () => {
           const isPositive = percentageChange > 0;
           const changeText = isPositive ? `${percentageChange}%` : `${percentageChange}%`;
           const changeColor = isPositive ? 'text-green-500' : 'text-red-500';
-          const changeBorder = isPositive ?'border-green-500' : 'border-red-500';
+          const changeBorder = isPositive ? 'border-green-500' : 'border-red-500';
 
           // Add ref to the array
           const cardRef = (el) => {
@@ -103,17 +106,17 @@ const Dashboard = () => {
           // Handle hover effect
           const handleMouseCardEnter = () => {
             if (cardRefs.current[index]) {
-              cardRefs.current[index].style.borderWidth = '2px';
               cardRefs.current[index].style.borderColor = isPositive ? 'green' : 'red';
             }
           };
 
           const handleMouseCardLeave = () => {
             if (cardRefs.current[index]) {
-              cardRefs.current[index].style.borderWidth = '2px'; // Ensure the border width remains consistent
               cardRefs.current[index].style.borderColor = 'transparent';
             }
           };
+
+
 
           return (
             <Link
@@ -140,8 +143,19 @@ const Dashboard = () => {
                   </div>
                 </div>
               </div>
-              <div className="flex">
+              <div className="flex gap-4">
                 <button className={`${changeColor} text-center w-[80px] py-1 border-2 ${changeBorder} rounded-3xl`}>{changeText}</button>
+                <div className={`flex items-center border-2 rounded-full ${changeBorder} w-[40px] h-[40px]`}>
+                  <div className={`flex justify-center items-center cursor-pointer w-[40px] rounded-full ${changeColor} `}>
+                    {isPositive && (<FaArrowTrendUp size={22} />)}
+                    {!isPositive && (<FaArrowTrendDown size={22} />)}
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col mt-6 gap-3">
+                <h1 className={`${changeColor} text-xl`}>${coin.current_price}</h1>
+                <h1 className="text-lg text-gray-400">Total Volume: {coin.total_volume}</h1>
+                <h1 className="text-lg text-gray-400">Market Cap: ${coin.market_cap}</h1>
               </div>
             </Link>
           );
