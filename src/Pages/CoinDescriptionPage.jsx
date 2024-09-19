0c62b78edffb8b51620aedecad759331d48f6859
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-09-19 14:03:01 +0530

    Made Some Changes

--- src/Pages/CoinDescriptionPage.jsx
+++ src/Pages/CoinDescriptionPage.jsx
@@ -17,6 +17,7 @@ const CoinDescriptionPage = () => {
   const changeText = isPositive ? `${percentageChange}%` : `${percentageChange}%`;
 
   const changeColor = isPositive ? 'text-green-500' : 'text-red-500';
+  const changeBackground = isPositive ? 'bg-green-500' : 'bg-red-500';
   const changeBorder = isPositive ? 'border-green-500' : 'border-red-500';
 
 
@@ -46,12 +47,18 @@ const CoinDescriptionPage = () => {
               <h1 className='lg:text-md text-sm text-gray-600'>{coinData.name}</h1>
             </div>
           </div>
-          <div className="flex items-center lg:gap-4 gap-2 justify-start">
-            <button className={`${changeColor} text-center xl:w-[80px] lg:text-md text-sm w-[60px] py-1 border-2 ${changeBorder} rounded-3xl sm:block hidden`}>{changeText}</button>
-            <div className={`xl:flex hidden items-center border-2 rounded-full ${changeBorder} w-[40px] h-[40px]`}>
-              <div className={`lg:flex hidden justify-center items-center cursor-pointer w-[40px] rounded-full ${changeColor} `}>
-                {isPositive && (<FaArrowTrendUp size={22} />)}
-                {!isPositive && (<FaArrowTrendDown size={22} />)}
+          <div className="flex gap-4">
+            <div className="group flex items-center lg:gap-4 gap-2 justify-start">
+              <div className="group relative">
+                <button className={`${changeColor} group-hover:bg-current text-center xl:w-[80px] lg:text-md text-sm w-[60px] py-1 border-2 ${changeBorder} rounded-3xl sm:block hidden`}>{changeText}</button>
+                <button className={`${changeColor} top-0 left-0 absolute group-hover:opacity-100 group-hover:text-white text-center xl:w-[80px] lg:text-md text-sm w-[60px] py-1 border-2 ${changeBorder} rounded-3xl sm:block hidden transition-colors`}>{changeText}</button>
+              </div>
+            </div>
+            <div className="group">
+              <div className={`xl:flex hidden items-center border-2 rounded-full ${changeBorder} w-[40px] h-[40px] relative`}>
+                <div className={`absolute w-full h-full top-0 left-0 ${changeBackground} opacity-0 group-hover:opacity-100 transition-all rounded-full`}></div>
+                {isPositive && (<FaArrowTrendUp className={`group-hover:text-white absolute top-2 left-1 ${changeColor}`} size={22} />)}
+                {!isPositive && (<FaArrowTrendDown className={`group-hover:text-white absolute top-2 left-1 ${changeColor}`} size={22} />)}
               </div>
             </div>
           </div>
@@ -62,17 +69,24 @@ const CoinDescriptionPage = () => {
               Current Price
             </span>
           </div>
-          <h1 className={`lg:text-lg ${theme === 'dark' ? 'text-white' : 'text-black'}`}>${coinData.total_volume.toLocaleString()}</h1>
-          <h1 className={`lg:text-lg ${theme === 'dark' ? 'text-white' : 'text-black'}`}>${coinData.market_cap.toLocaleString()}</h1>
-          <div className={`flex items-center border-2 rounded-full ${changeBorder} w-[40px] h-[40px]`}>
-            <div className={`flex justify-center items-center cursor-pointer w-[40px] rounded-full`}>
-              <div className="group relative">
-                <FaRegStar className={`${changeColor}`} size={22} />
-                <span
-                            className={`text-sm ${theme==='dark'?'bg-gray-400':'bg-gray-200'} rounded-md px-2 pointer-events-none absolute -top-10 -left-12 w-max opacity-0 transition-opacity group-hover:opacity-100 `}>
-                  Add to Watchlist
-                </span>
-              </div>
+          <div className="group relative">
+            <button className={`lg:text-lg ${theme === 'dark' ? 'text-white' : 'text-black'}`}>${coinData.total_volume.toLocaleString()}</button>
+            <span
+              className={`text-sm ${theme === 'dark' ? 'bg-gray-400' : 'bg-gray-200'} rounded-md px-2 pointer-events-none absolute -top-7 left-2 w-max opacity-0 transition-opacity group-hover:opacity-100 `}>
+              Total Volume
+            </span>
+          </div>
+          <div className="group relative">
+            <button className={`lg:text-lg ${theme === 'dark' ? 'text-white' : 'text-black'}`}>${coinData.market_cap.toLocaleString()}</button>
+            <span
+              className={`text-sm ${theme === 'dark' ? 'bg-gray-400' : 'bg-gray-200'} rounded-md px-2 pointer-events-none absolute -top-7 left-4 w-max opacity-0 transition-opacity group-hover:opacity-100 `}>
+              Market Cap
+            </span>
+          </div>
+          <div className={`flex items-center border-2 rounded-full ${changeBorder} lg:w-[40px] lg:h-[40px] h-[25px] w-[25px] group relative`}>
+            <div className={`absolute w-full h-full top-0 left-0 ${changeBackground} opacity-0 group-hover:opacity-100 transition-all rounded-full`}></div>
+            <div className={`flex justify-center items-center cursor-pointer w-[25px] lg:w-[40px] rounded-full`}>
+              <FaRegStar className={`${changeColor} group-hover:text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all`} size={22} />
             </div>
           </div>
         </div>
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -140,6 +140,7 @@ const PaginatedDashboard = ({ noOfCoinsPerPage }) => {
             const changeText = isPositive ? `${percentageChange}%` : `${percentageChange}%`;
             const changeColor = isPositive ? 'text-green-500' : 'text-red-500';
             const changeBorder = isPositive ? 'border-green-500' : 'border-red-500';
+            const changeBackground = isPositive ? 'bg-green-500' : 'bg-red-500';
 
             // Add ref to the array
             const cardRef = (el) => {
@@ -180,27 +181,27 @@ const PaginatedDashboard = ({ noOfCoinsPerPage }) => {
                     </div>
                   </div>
                   {gridLayout && (
-                    <div className={`flex items-center border-2 rounded-full ${changeBorder} w-[40px] h-[40px]`}>
-                      <div className={`flex justify-center items-center cursor-pointer w-[40px] rounded-full`}>
-                        <div className="group relative">
-                          <FaRegStar className={`${changeColor}`} size={22} />
-                          <span
-                            className={`text-sm ${theme === 'dark' ? 'bg-gray-400' : 'bg-gray-200'} rounded-md px-2 pointer-events-none absolute -top-10 -left-10 w-max opacity-0 transition-opacity group-hover:opacity-100 `}>
-                            Add to Watchlist
-                          </span>
-                        </div>
-
+                    <div className={`flex items-center border-2 rounded-full ${changeBorder} lg:w-[40px] lg:h-[40px] h-[25px] w-[25px] group relative`}>
+                      <div className={`absolute w-full h-full top-0 left-0 ${changeBackground} opacity-0 group-hover:opacity-100 transition-all rounded-full`}></div>
+                      <div className={`flex justify-center items-center cursor-pointer w-[25px] lg:w-[40px] rounded-full`}>
+                        <FaRegStar className={`${changeColor} group-hover:text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all`} size={22} />
                       </div>
                     </div>
                   )
                   }
                 </div>
-                <div className="flex items-center lg:gap-4 gap-2 justify-start">
-                  <button className={`${changeColor} text-center xl:w-[80px] lg:text-md text-sm w-[60px] py-1 border-2 ${changeBorder} rounded-3xl sm:block hidden`}>{changeText}</button>
-                  <div className={`xl:flex hidden items-center border-2 rounded-full ${changeBorder} w-[40px] h-[40px]`}>
-                    <div className={`lg:flex hidden justify-center items-center cursor-pointer w-[40px] rounded-full ${changeColor} `}>
-                      {isPositive && (<FaArrowTrendUp size={22} />)}
-                      {!isPositive && (<FaArrowTrendDown size={22} />)}
+                <div className="flex gap-4">
+                  <div className="group flex items-center lg:gap-4 gap-2 justify-start">
+                    <div className="group relative">
+                      <button className={`${changeColor} group-hover:bg-current text-center xl:w-[80px] lg:text-md text-sm w-[60px] py-1 border-2 ${changeBorder} rounded-3xl sm:block hidden`}>{changeText}</button>
+                      <button className={`${changeColor} top-0 left-0 absolute group-hover:opacity-100 group-hover:text-white text-center xl:w-[80px] lg:text-md text-sm w-[60px] py-1 border-2 ${changeBorder} rounded-3xl sm:block hidden transition-colors`}>{changeText}</button>
+                    </div>
+                  </div>
+                  <div className="group">
+                    <div className={`xl:flex hidden items-center border-2 rounded-full ${changeBorder} w-[40px] h-[40px] relative`}>
+                      <div className={`absolute w-full h-full top-0 left-0 ${changeBackground} opacity-0 group-hover:opacity-100 transition-all rounded-full`}></div>
+                      {isPositive && (<FaArrowTrendUp className={`group-hover:text-white absolute top-2 left-1 ${changeColor}`} size={22} />)}
+                      {!isPositive && (<FaArrowTrendDown className={`group-hover:text-white absolute top-2 left-1 ${changeColor}`} size={22} />)}
                     </div>
                   </div>
                 </div>
@@ -228,9 +229,10 @@ const PaginatedDashboard = ({ noOfCoinsPerPage }) => {
                   {!gridLayout && (<h1 className={`lg:text-lg ${gridLayout ? 'text-lg' : 'text-sm'} ${theme === 'dark' ? 'text-white' : 'text-black'} md:block hidden`}>${formatNumber(coin.total_volume)}</h1>)}
                   {!gridLayout && (<h1 className={`lg:text-lg ${gridLayout ? 'text-lg' : 'text-sm'} first-line:${theme === 'dark' ? 'text-white' : 'text-black'}`}>${formatNumber(coin.market_cap)}</h1>)}
                   {!gridLayout && (
-                    <div className={`flex items-center border-2 rounded-full ${changeBorder} lg:w-[40px] lg:h-[40px] h-[25px] w-[25px]`}>
-                      <div className={`flex justify-center items-center cursor-pointer w-[25px] lg:w-[40px] rounded-full ${changeColor} `}>
-                        <FaRegStar className={`${changeColor}`} size={22} />
+                    <div className={`flex items-center border-2 rounded-full ${changeBorder} lg:w-[40px] lg:h-[40px] h-[25px] w-[25px] group relative`}>
+                      <div className={`absolute w-full h-full top-0 left-0 ${changeBackground} opacity-0 group-hover:opacity-100 transition-all rounded-full`}></div>
+                      <div className={`flex justify-center items-center cursor-pointer w-[25px] lg:w-[40px] rounded-full`}>
+                        <FaRegStar className={`${changeColor} group-hover:text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all`} size={22} />
                       </div>
                     </div>
                   )
